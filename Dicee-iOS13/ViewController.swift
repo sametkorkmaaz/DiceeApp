ae1885f7853eb2f8fdfd6292c71ad6d0a0e5cc06
Samet Korkmaz <115614473+sametkorkmaaz@users.noreply.github.com>
2023-09-10 16:07:44 +0300

    Update ViewController.swift

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,10 +1,8 @@
 //
-//  ViewController.swift
+//  File.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
+//  Created by Samet Korkmaz on 10.09.2023.
 
 import UIKit
 
@@ -12,13 +10,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
+    // viewlara control ile tutup burada tanımladık
+
+    override func viewDidLoad() { // görüntü yüklenince olacaklar
+        super.viewDidLoad()
+    }
+    // butonu tanımlıyoruz
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-        
-        diceImageView1.image = diceArray[Int.random(in: 0...5)]
-        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        // zar imagelerini dizi içerisine attık
+        // #imageLiteral() imageleri elle seçmemizi sağlar
+
+        // 2 şekilde rastgeşe sayı üretilebilir
+        diceImageView1.image = diceArray.randomElement() // dizinin elemanları içerisinden rastgele eleman seçer
+        diceImageView2.image = diceArray[Int.random(in: 0...5)] // 0-5 arasında rastgele integer değer verir
         
     }
     
